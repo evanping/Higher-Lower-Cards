38b254a92a6701e779a79930403dad404bcff230
hudyu17 <hengjeung.yuen@gmail.com>
2023-08-06 10:08:33 -0400

    plausible tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.svg"/>
-        <PlausibleProvider domain="https://mlb-higher-lower-v2.vercel.app" />
+        <PlausibleProvider domain="mlb-higher-lower-v2.vercel.app" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
