05b223c63a3e716504628f7853036a06a058f407
Evan Ping <evanping88@gmail.com>
2024-08-03 18:46:53 -0400

    improved shuffling algorithm

--- src/app/util/cardService.tsx
+++ src/app/util/cardService.tsx
@@ -2,13 +2,13 @@ import data from "../../data/card-bank.json";
 
 export default function getCards() {
   let cardArray: Array<any> = [...data];
-  cardArray.sort(() => Math.random() - 0.5); // shuffle order of cards
+  //cardArray.sort(() => Math.random() - 0.5); // shuffle order of cards
 
-  // let allCards: Array<any> = []
-  // while (cardArray.length >= 2) {
-  //     allCards.push(cardArray.splice(0, 2)) // inserts cards in pairs
-  // }
+  // Fisher-Yates shuffle
+  for (let i = cardArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cardArray[i], cardArray[j]] = [cardArray[j], cardArray[i]];
+  }
 
-  // return allCards;
   return cardArray;
 }
